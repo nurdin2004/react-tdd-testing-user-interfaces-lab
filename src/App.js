a75eab0ccd91e53d88aa70e6c276a9d433f82eb4
nurdin2004 <adiaderkisohouf@gmail.com>
2025-04-26 17:10:05 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
 function App() {
-  return <div>Your code here</div>;
+  return (
+    <div>
+      <h1>Hi, I'm JARED</h1>
+      <img
+      src="https://avatars.githubusercontent.com/u/198506538?s=400&u=02fb5de6b061cb7375272b3bba8b2b2b4307800b&v=4"
+       alt="Photo of Jared smiling"
+      />
+      <h2>About Me</h2>
+
+      <p>
+         I'm a passionate developer who loves building and doing deep research.
+       </p>
+       <a href="https://github.com/dashboard" >
+        Github 
+       </a>
+        <br />
+        <a href="https://www.linkedin.com/mynetwork/grow/">
+         LinkedIn
+        </a>
+    </div>
+  );
 }
 
 export default App;
